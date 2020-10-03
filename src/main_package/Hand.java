045d2ccd15f50e0bee6def98d14b381b6e2b6693
PileichMasha <pileichmaria2001@gmail.com>
2020-10-03 20:51:12 +0300

    Добавила проверку на флеш и стрит

--- src/main_package/Hand.java
+++ src/main_package/Hand.java
@@ -1,6 +1,6 @@
 package main_package;
 
-import java.util.Set;
+import java.util.Arrays;
 
 public class Hand {
     private Card[] cards;
@@ -9,35 +9,88 @@ public class Hand {
 
     public Hand(Deck deck) {
         this.cards = new Card[5];
-        this.ranksInHand = new int[14];  //оставить [0] пустым
+        this.ranksInHand = new int[15];  //[0], [1] пустые
         this.suitsInHand = new int[4];
 
         for (int i = 0; i < 5; i++) {
             cards[i] = deck.drawFromDeck();  //раздача карт
         }
-        ranksInHand = this.countRanks();  //будет работать ?
+        countRanks();
     }
 
-    private int[] countRanks() {             //или void ?
-        for (int i = 0; i <= 13 ; i++) {
+    private void countRanks() {
+        for (int i = 0; i <= 14 ; i++) {
             ranksInHand[i] = 0;
         }
         for (int i = 0; i <= 4; i++) {
             ranksInHand[cards[i].getRank()]++;
         }
+    }
+    
+    public boolean isFlush() {   //проверка на флеш
+        boolean flush = true;
+        for (int i = 0; i < 4; i++) {
+            if (cards[i].getSuit() != cards[i + 1].getSuit()) {
+                flush = false;
+                break;
+            }
+        }
+        return flush;
+    }
+
+    public boolean isStraight() {  //проверка на стрит
+        Arrays.sort(cards);
+        if (cards[cards.length - 1].getRank() - cards[0].getRank() == 4)
+            return true;
+        else return false;
+    }
+
+    public void checkRanks() {
+        int maxRep = 1; int minRep = 1;  //кол-во повторений
+        int maxPos = -1; int minPos;     //ранг, который повторяется
+        for (int i = 2; i < 15; i++) {
+            if (ranksInHand[i] != 0) {
+                if (ranksInHand[i] == maxRep) {  //не учитывает два одинаковых следующих друг за другом ранга
+                    maxPos = i;
+                } else if (ranksInHand[i] > maxRep) {
+                    minRep = maxRep; minPos = maxPos;
+                    maxRep = ranksInHand[i]; maxPos = i;
+                }
+            }
+        }
+
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+
+    public int[] getSuitsInHand() {
+        return suitsInHand;
+    }
+
+    public int[] getRanksInHand() {
         return ranksInHand;
     }
 
-    public void findSameRanks() {
-        int sameCards = 1;        //ищем ранг, повторившийся наибольшее кол-во раз
-        int rank = -1;
-        for (int i = 13; i >= 1 ; i--) {
-            if (ranksInHand[i] > sameCards) {
-                sameCards = ranksInHand[i];
-                rank = i;                     //карты какого ранга повторились
+    /*public void findSameRanks() {
+        int sameRank1 = 1;        //кол-во повторяющихся карт
+        int sameRank2 = 1;
+        int smallRankPos = 0; int bigRankPos = 0;     //карты какого ранга повторились
+        for (int i = 14; i >= 2 ; i--) {
+            if (ranksInHand[i] > sameRank1) {
+                //sameRank1 = ranksInHand[i];
+                if (sameRank1 != 1) {
+                    sameRank2 = sameRank1;
+                    smallRankPos = bigRankPos;
+                    //sameRank1 = ranksInHand[i];
+                }
+                sameRank1 = ranksInHand[i];
+                bigRankPos = i;
+            } else if (ranksInHand[i] > sameRank2) {
+                sameRank2 = ranksInHand[i];
+                smallRankPos = i;
             }
         }
-    }
-
-
+    }*/
 }
